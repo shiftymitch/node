6eb739eae300e3abef57a19cd532319fdf06b803
shiftymitch <shiftymitch@gmail.com>
2020-04-23 11:08:24 -0600

    added badge

--- 9/index.js
+++ 9/index.js
@@ -109,6 +109,8 @@ ${answers.test}
 ## Questions
 Questions can be directed via email to ${answers.email}
 
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+
     `;
 }
 
