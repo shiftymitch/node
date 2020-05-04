64ec6353b6044f11b81a45d2802305fe0cccf78a
Mitch Henderson <shiftymitch@gmail.com>
2020-05-04 07:02:59 -0600

    Employee Test Passed

--- 10/Develop/lib/Employee.js
+++ 10/Develop/lib/Employee.js
@@ -1,7 +1,22 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email, role) {
-        
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.getName = function() {
+            return this.name;
+        };
+        this.getId = function() {
+            return this.id;
+        };
+        this.getEmail = function() {
+            return this.email;
+        };
+        this.getRole = function() {
+            return "Employee";
+        };
     }
 }
 
