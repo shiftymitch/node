761558c250f812304b088fbc5a43097c1ae5ca06
Mitch Henderson <shiftymitch@gmail.com>
2020-05-04 07:01:37 -0600

    initial commit

---
+++ 10/Develop/app.js
@@ -0,0 +1,122 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+let employee = "";
+
+// initial prompt questions
+function initialQuestions() {
+    console.log("Add new Team Member:")
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Name: "
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Employee ID: "
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Email Address: "
+            },
+            {
+                type: "list",
+                name: "role",
+                message: "Role: ",
+                choices: ["Manager", "Engineer", "Intern"]
+            }
+        ]);
+}
+
+//class specific add-on questions
+function managerQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Office Number: "
+        }
+    ]);
+
+}
+
+function engineerQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "Github Username: "
+        }
+    ]);
+}
+
+function internQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "school",
+            message: "School: "
+        }
+    ]);
+}
+
+
+initialQuestions()
+    .then(function(answers) {
+
+        //role switcher
+        switch(answers.role) {
+            case "Manager":
+                managerQuestions();
+                employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                return;
+            case "Engineer":
+                engineerQuestions();
+                employee = new Engineer(answers.name, answers.id, answers.email, answers.github);
+                return;
+            case "Intern":
+                internQuestions();
+                employee = new Intern(answers.name, answers.id, answers.email, answers.school);
+                return;
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+
+
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
