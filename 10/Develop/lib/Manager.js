4173e47b18e218a45f1a2e596324bafc3687898f
Mitch Henderson <shiftymitch@gmail.com>
2020-05-04 07:04:09 -0600

    employee type classes

--- 10/Develop/lib/Manager.js
+++ 10/Develop/lib/Manager.js
@@ -1,1 +1,10 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, phone) {
+        
+    }
+};
+
+module.exports = Manager;
